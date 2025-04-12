9b3e50e31de2ac5810620d641058554e6fad3184
Alan Sousa <alanssrv@outlook.com>
2025-04-11 21:28:41 -0300

    feat: tela de visualizacao de formacoes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './modules/pages/home/home.component';
 import { NotFoundComponent } from './modules/pages/not-found/not-found.component';
 import { AboutComponent } from './modules/pages/about/about.component';
 import { ExperiencesComponent } from './modules/pages/experiences/experiences.component';
+import { FormationsComponent } from './modules/pages/formations/formations.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,10 @@ export const routes: Routes = [
     {
         path: 'experiences',
         component: ExperiencesComponent
+    },
+    {
+        path: 'formations',
+        component: FormationsComponent
     },
     {
         path: '**',
