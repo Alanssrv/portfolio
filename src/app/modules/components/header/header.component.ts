8e24463e243ccbc51e2532101bfaaa7066b492b1
Alan Sousa <alanssrv@gmail.com>
2026-04-11 22:11:40 -0300

    feat: adicionando botoes de mudanca de idioma no header

--- src/app/modules/components/header/header.component.ts
+++ src/app/modules/components/header/header.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
+  animations: [
+    trigger('rotateIcon', [
+      state('open', style({
+        transform: 'rotate(180deg)'
+      })),
+      state('closed', style({
+        transform: 'rotate(0deg)'
+      })),
+      transition('open <=> closed', [
+        animate('300ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
 
+  public isShowMoreMenu = false;
+  public selectedLanguage = 1;
+
+  toggleMore() {
+    this.isShowMoreMenu = !this.isShowMoreMenu;
+  }
+
+  changeLanguage(idLanguage: number) {
+    this.selectedLanguage = idLanguage;
+  }
 }
